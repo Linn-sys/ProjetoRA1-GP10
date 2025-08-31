1c03f9bdd74d25a7b2e230375a640ff3cafc62ab
Caliel <206436051+Linn-sys@users.noreply.github.com>
2025-08-31 12:40:41 -0300

    Overhaul

--- Backend/Pipes/Pipes.cpp
+++ Backend/Pipes/Pipes.cpp
@@ -1,4 +1,4 @@
-// RA1ProjC++.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+// Pipes.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
 //
 
 #include <iostream>
